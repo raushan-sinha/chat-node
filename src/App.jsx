1a75214d262e21140c940e79fddc6c31053f9f5c
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-10-18 13:22:28 +0530

    Files are uploaded

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+    return (
+        <>
+            <h1 className='text-red-500 font-semibold font-mono text-3xl underline'>ChatNode</h1>
+        </>
+    );
+}
